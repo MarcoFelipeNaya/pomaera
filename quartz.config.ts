c1ece7b10be6a0489921697bd28cd1a2d222d0c1
MarcoNaya <marco.gntz@gmail.com>
2024-08-21 21:17:56 -0300

    Quartz sync: Aug 21, 2024, 9:17 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "marcofelipenaya.github.io/pomaera/",
+    baseUrl: "quartz.jzhao.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
